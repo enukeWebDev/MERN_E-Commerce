bdf0f0703c196521b5f5b26bec8de6385121cf42
Roderick <enuke4@gmail.com>
2022-03-22 10:12:35 -0600

    change style using react bootstrap

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ This project is an E-Commerce using MERN Application. This is currently on-going
 - create server at the backend - complete
 - access data from backend and render to frontend - complete
 - use Reducer to manage complexity on home screen - complete
+- modify styles using React Bootstrap UI Framework -
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <Router>
       <div>
         <header>
-          <Link to="/">La_Vendor</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>La_Vendor</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
 
         <main>
@@ -15,26 +24,6 @@ function App() {
             <Route path="/product/:slug" element={<ProductScreen />} />
             <Route path="/" element={<HomeScreen />} />
           </Routes>
-
-          {/* <h1>Feautured Products</h1>
-          <div className="products">
-            {data.products.map((product) => (
-              <div className="product" key={product.slug}>
-                <a href={`product/${product.slug}`}>
-                  <img src={product.image} alt={product.name}></img>
-                </a>
-                <div className="product-info">
-                  <a href={`product/${product.slug}`}>
-                    <p>{product.name}</p>
-                  </a>
-                  <p>
-                    <strong>${product.price}</strong>
-                  </p>
-                  <button>Add To Cart</button>
-                </div>
-              </div>
-            ))}
-          </div> */}
         </main>
       </div>
     </Router>
