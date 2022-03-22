7d0bc1847ba0a79bb5ee7ba60a324387d823e0d6
Roderick <enuke4@gmail.com>
2022-03-22 10:23:26 -0600

    create header, footer using react bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 function App() {
   return (
     <Router>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
           <Navbar bg="dark" variant="dark">
             <Container>
@@ -20,11 +20,17 @@ function App() {
         </header>
 
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+
+        <footer>
+          <div className="text-center">All Rights Reserved</div>
+        </footer>
       </div>
     </Router>
   );
