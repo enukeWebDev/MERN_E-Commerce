c7adfc08511e85ed7a559d6cf70e2b1fa199793c
Roderick <enuke4@gmail.com>
2022-03-21 21:05:19 -0600

    create the sample data and render to main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import data from './data';
+
 function App() {
   return (
     <div>
@@ -5,7 +7,27 @@ function App() {
         <a href="/">La_Vendor</a>
       </header>
 
-      <main>list products</main>
+      <main>
+        <h1>Feautured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`product/${product.slug}`}>
+                <img src={product.image} alt={product.name}></img>
+              </a>
+              <div className="product-info">
+                <a href={`product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add To Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,57 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim Shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+
+    {
+      name: 'Addidas Slim Shirt',
+      slug: 'addidas-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpg',
+      price: 220,
+      countInStock: 20,
+      brand: 'Addidas',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+
+    {
+      name: 'Nike Fit Pant',
+      slug: 'nike-fit-pant',
+      category: 'Pants',
+      image: '/images/p3.jpg',
+      price: 20,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+
+    {
+      name: 'Addidas Fit Pant',
+      slug: 'addidas-fit-pant',
+      category: 'Pants',
+      image: '/images/p4.jpg',
+      price: 60,
+      countInStock: 12,
+      brand: 'Addidas',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+  ],
+};
+
+export default data;
