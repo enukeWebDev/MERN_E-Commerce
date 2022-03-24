ca37b5c7c63d132919d9b53f200c09c684f80ef3
Roderick <enuke4@gmail.com>
2022-03-24 11:46:35 -0600

    implement add to cart functionality to homescreen

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Rating from './Rating';
+import axios from 'axios';
+import { Store } from '../Store';
 
 function Product(props) {
   const { product } = props;
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+
+  const addToCartHandler = async (item) => {
+    const existItem = cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${item._id}`);
+
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product is out of Stock!');
+      return;
+    }
+
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...item, quantity },
+    });
+  };
 
   return (
     <Card>
@@ -27,7 +50,7 @@ function Product(props) {
         ></Rating>
 
         <Card.Text>${product.price}</Card.Text>
-        <Button>Add To Cart</Button>
+        <Button onClick={() => addToCartHandler(product)}>Add To Cart</Button>
       </Card.Body>
     </Card>
   );
