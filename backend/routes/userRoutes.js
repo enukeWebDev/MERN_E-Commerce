b955ed9e407f9d14c6c78f184a6ff567c866656c
Roderick <enuke4@gmail.com>
2022-03-29 09:53:51 -0600

    add dependencies

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import User from '../models/userModel.js';
 import bcrypt from 'bcryptjs';
+import { generateToken } from '../utils.js';
+import expressAsyncHandler from 'express-async-handler';
 
 const userRouter = express.Router();
 
@@ -17,6 +19,7 @@ userRouter.post(
           name: user.name,
           email: user.email,
           isAdmin: user.isAdmin,
+          token: generateToken(user),
         });
         return;
       }
@@ -24,3 +27,5 @@ userRouter.post(
     res.status(401).send({ message: 'Ivalid email or password!' });
   })
 );
+
+export default userRouter;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -18,8 +19,14 @@ mongoose
   });
 
 const app = express();
+
+// The form data in the post request will be converted to json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
 
 // Error handler for express
 app.use((err, req, res, next) => {
