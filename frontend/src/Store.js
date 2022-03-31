bb11edbb751ca03a7f8f677faf5f6d2956d3cfbd
Roderick <enuke4@gmail.com>
2022-03-30 18:59:55 -0600

    store information on form to local storage

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -8,6 +8,10 @@ const initialState = {
     : null,
 
   cart: {
+    shippingAddress: localStorage.getItem('shippingAddress')
+      ? JSON.parse(localStorage.getItem('shippingAddress'))
+      : {},
+
     cartItems: localStorage.getItem('cartItems')
       ? JSON.parse(localStorage.getItem('cartItems'))
       : [],
@@ -54,6 +58,15 @@ function reducer(state, action) {
       return {
         ...state,
         userInfo: null,
+      };
+
+    case 'SAVE_SHIPPING_ADDRESS':
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          shippingAddress: action.payload,
+        },
       };
 
     default:
