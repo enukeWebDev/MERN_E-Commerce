e5181dbe3d63e7b819f6a147fdd3a3747aaa31e7
Roderick <enuke4@gmail.com>
2022-03-31 18:28:08 -0600

    complement functionality of payment method screen

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -12,6 +12,10 @@ const initialState = {
       ? JSON.parse(localStorage.getItem('shippingAddress'))
       : {},
 
+    paymentMethod: localStorage.getItem('paymentMethod')
+      ? localStorage.getItem('paymentMethod')
+      : '',
+
     cartItems: localStorage.getItem('cartItems')
       ? JSON.parse(localStorage.getItem('cartItems'))
       : [],
@@ -61,6 +65,7 @@ function reducer(state, action) {
         cart: {
           cartItems: [],
           shippingAddress: {},
+          paymentMethod: '',
         },
       };
 
@@ -70,6 +75,15 @@ function reducer(state, action) {
         cart: {
           ...state.cart,
           shippingAddress: action.payload,
+        },
+      };
+
+    case 'SAVE_PAYMENT_METHOD':
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          paymentMethod: action.payload,
         },
       };
 
--- frontend/src/screens/PaymentMethodScreen.js
+++ frontend/src/screens/PaymentMethodScreen.js
@@ -1,14 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 import CheckoutSteps from '../components/CheckoutSteps';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { Store } from '../Store';
+import { useNavigate } from 'react-router-dom';
 
 function PaymentMethodScreen() {
-  const [paymentMethodName, setPaymentMethodName] = useState('');
+  const navigate = useNavigate();
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+
+  const {
+    cart: { shippingAddress, paymentMethod },
+  } = state;
+
+  const [paymentMethodName, setPaymentMethodName] = useState(
+    paymentMethod || 'PayPal'
+  );
+
+  useEffect(() => {
+    if (!shippingAddress.address) {
+      navigate('/shipping');
+    }
+  }, [shippingAddress, navigate]);
 
   const submitHandler = (e) => {
     e.preventDefault();
+
+    ctxDispatch({
+      type: 'SAVE_PAYMENT_METHOD',
+      payload: {
+        paymentMethodName,
+      },
+    });
+
+    localStorage.setItem('paymentMethod', paymentMethodName);
+    navigate('/placeorder');
   };
 
   return (
